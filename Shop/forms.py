189bdc026bf14e56e2602c4ae3873cb7b3d333a4
Jobayrul Hasan <jobayerdpi11@gmail.com>
2025-09-21 22:19:31 +0600

    last changes

--- Shop/forms.py
+++ Shop/forms.py
@@ -1,7 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.utils.translation import gettext, gettext_lazy as _
 
 # user registration form. django default
 class CustomerRegistrationForm(UserCreationForm):
@@ -19,8 +18,3 @@ class CustomerRegistrationForm(UserCreationForm):
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
         }
-        
-# login form. django default
-# class LoginForm(AuthenticationForm):
-#     username = UsernameField(widget=forms.TextInput(attrs={'autofocus':True,'class':'form-control' }))
-#     password = forms.CharField(label=_('Password'),strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'current_password','class':'form-control' }))
